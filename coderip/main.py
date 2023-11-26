a657d65c225a076991539798e5da011db90bff90
Richard Abrich <richard.abrich@gmail.com>
2023-11-26 11:29:46 -0500

    fix typo

--- coderip/main.py
+++ coderip/main.py
@@ -177,7 +177,7 @@ multiple (2+) LLM threads implementing separate "aspects"
         - system commands
     - dynamic
         e.g. "modify the source code to implement the developer's goal: <user_goal = ask_user("what should we do next?")
-        e.g. "implement a state machine. here are the states and transition functions: ..."""
+        e.g. "implement a state machine. here are the states and transition functions: ..."
         e.g. "take the output from the previous message and extract the following json:
 
 
